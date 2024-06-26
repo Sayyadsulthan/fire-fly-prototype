300ef69d106035752ea91f5baf23bf37d417f1be
Sayyadsulthan <asulthan088@gmail.com>
2024-06-26 14:02:45 +0530

    fixed bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Frame6 from '../assets/images/Frame 16.png';
+import ProductImg from '../assets/images/ProductImg.png';
 
 const Header = ({ isActive, handleVisibleCard }) => {
     return (
         <div
             className='header '
-            style={{ backgroundImage: `url(${Frame6})`, filter: isActive && 'blur(5px)' }}
+            style={{
+                backgroundImage: `url(${ProductImg || Frame6})`,
+                filter: isActive && 'blur(5px)',
+            }}
         >
             {/* <img src={Frame6} alt="" srcset="" /> */}
             <div></div>
